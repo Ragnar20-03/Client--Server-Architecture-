3b599a4445eb6bfacf3c3e718a88f2089330568a
Ragnar <roshanpp20@gmail.com>
2023-04-22 10:22:16 +0530

    3

--- Sever.java
+++ Sever.java
@@ -5,6 +5,10 @@ class Client
 {
     public static void main (String Arg[])
     {
-        System.out.println("Client Appliaction is RUnning..")
+        System.out.println("Server Appliaction is RUnning..")
+        ServerSocket ssobj=new ServerSocket(2100);
+        System.out.println("Server is running at port no : 2100" )
+
+        Socket s = ssobj.accept();
     }
 }
