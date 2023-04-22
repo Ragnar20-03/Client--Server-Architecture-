a6d51949006b60e4f032aed7cac4dd27b3e2e43c
Ragnar <roshanpp20@gmail.com>
2023-04-22 10:51:52 +0530

    Susccesful connection

--- Client.java
+++ Client.java
@@ -1,13 +1,22 @@
 import java.net.*;
-import java.io.*;
+import java.io.*;  //Input Output Stream
 
 class Client
 {
-    public static void main (String Arg[])
+    public static void main (String Arg[]) throws Exception
     {
         System.out.println("Client Appliaction is RUnning..");
 
-        Socket s new Socket ("localhost",2100);
         System.out.println("Client is Waiting For Connection");
+        
+        Socket s = new Socket ("localhost",2100);
+
+        PrintStream ps = new PrintStream(s.getOutputStream());
+
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1,str2;
     }
 }
--- Server.java
+++ Server.java
@@ -1,9 +1,9 @@
 import java.net.*;
 import java.io.*;
 
-class Client
+class Server
 {
-    public static void main (String Arg[])
+    public static void main (String Arg[]) throws Exception
     {
         System.out.println("Server Appliaction is RUnning..");
         ServerSocket ssobj=new ServerSocket(2100);
@@ -11,5 +11,13 @@ class Client
 
         Socket s = ssobj.accept();
         System.out.println("Server and client connection is succesful");
+
+        PrintStream ps = new PrintStream(s.getOutputStream());
+
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1,str2;
     }
 }
